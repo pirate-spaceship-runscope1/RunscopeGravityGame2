c9701348093cdd59561926d82148210fda9dce95
pirate-spaceship-runscope1 <pirate-spaceship-runscope-1@outlook.com>
2023-08-01 22:42:58 +0200

    Delete appcenter-pre-build.sh

--- appcenter-pre-build.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-wget http://ftp.de.debian.org/debian/pool/main/o/openssl/libssl1.1_1.1.1n-0+deb11u5_amd64.deb && \
-dpkg -i libssl1.1_1.1.1n-0+deb11u5_amd64.deb && \
-wget http://ftp.de.debian.org/debian/pool/main/o/openssl/openssl_1.1.1n-0+deb11u5_amd64.deb && \
-dpkg -i openssl_1.1.1n-0+deb11u5_amd64.deb
